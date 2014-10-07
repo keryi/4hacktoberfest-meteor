ef7a302767cbaa0b08abe96e9573e9f0ec990331
keryi <hti_32@yahoo.com>
2014-10-08 05:06:47 +0800

    Grouped the order with same menu id

--- client/views/orders/order_submit.js
+++ client/views/orders/order_submit.js
@@ -5,10 +5,20 @@ Template.orderSubmit.events({
 		var order = {
 			menuId: $(e.target).find('[name=menu_id]').val(),
 			orderListId: Session.get('orderlist_id'),
-			quantity: $(e.target).find('[name=order_quantity]').val()
+			quantity: parseInt($(e.target).find('[name=order_quantity]').val())
 		}
 
-		order._id = Orders.insert(order);
+		var existing_order = Orders.findOne({
+			menuId: order.menuId,
+			orderListId: order.orderListId
+		});
+
+		if (existing_order != null) {
+			Orders.update({ _id: existing_order._id }, { $inc: { quantity: order.quantity } });
+		} else {
+			order._id = Orders.insert(order);
+		}
+
 		Router.go('orderLists', { _id: order.orderListId });
 	}
 })
