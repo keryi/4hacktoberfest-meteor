c4ba188b0fae30f79217b4d5ced8ca30fd11c985
keryi <hti_32@yahoo.com>
2014-10-10 20:58:12 +0800

    Make error messages disappear after seen by user

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -1,5 +1,9 @@
 Errors = new Meteor.Collection(null);
 
 throwError = function(message) {
-	Errors.insert({ message: message });
+	Errors.insert({ message: message, seen: false });
+}
+
+clearError = function() {
+	Errors.remove({ seen: true });
 }
