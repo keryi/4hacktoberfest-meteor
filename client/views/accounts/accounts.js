6e6e720a7c456f10d8251d6c565359fc063c3f0a
keryi <hti_32@yahoo.com>
2014-10-11 23:38:15 +0800

    Added signup validation

--- client/views/accounts/accounts.js
+++ client/views/accounts/accounts.js
@@ -1,3 +1,24 @@
+function checkUsername(username) {
+	if (username.length < 6) {
+		throwError("Username is too short, must be at least 6 characters");
+		return false;
+	}
+	return true;
+}
+
+function checkPassword(password, passwordConfirmation) {
+	if (password.length < 8) {
+		throwError("Password is too short, must be at least 8 characters");
+		return false;
+	}
+
+	if (passwordConfirmation != password) {
+		throwError("Password and password confirmation is not the same");
+		return false;
+	}
+	return true;
+}
+
 Template.signinForm.events({
 	'submit #signinForm': function(e, t) {
 		e.preventDefault();
@@ -21,18 +42,25 @@ Template.signupForm.events({
 	'submit #signupForm': function(e, t) {
 		e.preventDefault();
 
+		clearError();
+
 		var username = t.find('#signupUsername').value
 		var password = t.find('#signupPassword').value
 		var passwordConfirmation = t.find('#signupPasswordConfirmation').value
 
-		Accounts.createUser({
-			username: username,
-			password: password
-		}, function(error) {
-			if (error) {
-				throwError(error.reason);
-			}
-		});
+		var isValidUsername = checkUsername(username);
+		var isValidPassword = checkPassword(password, passwordConfirmation);
+
+		if (isValidUsername && isValidPassword) {
+			Accounts.createUser({
+				username: username,
+				password: password
+			}, function(error) {
+				if (error) {
+					throwError(error.reason);
+				}
+			});
+		}
 	},
 
 	'click #signin': function() {
