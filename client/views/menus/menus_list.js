984b3d90bc5e46ddd69628667df77dac5c45c3d4
keryi <hti_32@yahoo.com>
2014-10-26 18:27:05 +0800

    Use modal form to submit order and some UI stuff

--- client/views/menus/menus_list.js
+++ client/views/menus/menus_list.js
@@ -3,3 +3,45 @@ Template.menusList.helpers({
 		return Menus.find();
 	}
 });
+
+Template.menusList.events({
+	'click .order': function(e) {
+		e.preventDefault();
+
+		var menuId = $(e.target).attr('data-id');
+		Session.set('selectedMenuId', menuId);
+		$('#submit-order-modal').modal('show');
+	}
+});
+
+Template.submitOrderForm.helpers({
+	menu: function() {
+		var menuId = Session.get('selectedMenuId');
+		var menu = Menus.findOne({ _id: menuId });
+		return menu;
+	}
+});
+
+Template.submitOrderForm.events({
+	'click #order-confirm-button': function(e) {
+		e.preventDefault();
+
+		var order = {
+			menuId: $('#menu_id').val(),
+			orderListId: amplify.store('hacktoberfest.orderlist_id'),
+			quantity: parseInt($('#order_quantity').val()),
+			status: 'processing',
+			submitted: Date.now()
+		}
+
+		Meteor.call('order', order, function(error, id) {
+			if (error) {
+				Errors.throw(error.reason);
+			} else {
+				Router.go('orderLists', { _id: order.orderListId });
+			}
+		});
+
+		$('#submit-order-modal').modal('hide');
+	}
+});
--- client/views/order_lists/order_lists.js
+++ client/views/order_lists/order_lists.js
@@ -1,5 +1,52 @@
 Template.orderLists.helpers({
 	orders: function() {
 		return Orders.find({ orderListId: this._id });
+	},
+
+	menuItem: function() {
+		return Menus.findOne({ _id: this.menuId });
+	},
+
+	subtotal: function() {
+		var menuItem = Menus.findOne({ _id: this.menuId });
+		if (menuItem != null) {
+			return menuItem.price * this.quantity;
+		} else {
+			return 0.0;
+		}
+	},
+
+	editable: function() {
+		return this.status == 'processing';
+	},
+
+	orderStatusClass: function() {
+		switch (this.status) {
+			case 'processing':
+			return 'info';
+			case 'cooking':
+			return 'warning';
+			case 'served':
+			return 'success';
+			case 'reject':
+			return 'danger';
+		}
+	}
+});
+
+Template.orderLists.events({
+	'click .delete': function(e) {
+		e.preventDefault();
+
+		if (confirm('Cancel this order?')) {
+			var currentOrderId = this._id;
+			var currentOrderListId = this.orderListId;
+			Orders.remove(currentOrderId, function(error) {
+				if (error) {
+					Errors.throw(error.reason);
+				}
+			});
+			Router.go('orderLists', { _id: currentOrderListId });
+		}
 	}
 });
