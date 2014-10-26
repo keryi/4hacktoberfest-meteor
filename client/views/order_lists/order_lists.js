a71d28d11b56a72c027942dedb955096927d60eb
keryi <hti_32@yahoo.com>
2014-10-26 19:26:57 +0800

    Use modal form to edit order

--- client/views/order_lists/order_lists.js
+++ client/views/order_lists/order_lists.js
@@ -35,7 +35,7 @@ Template.orderLists.helpers({
 });
 
 Template.orderLists.events({
-	'click .delete': function(e) {
+	'click .delete-order': function(e) {
 		e.preventDefault();
 
 		if (confirm('Cancel this order?')) {
@@ -48,5 +48,53 @@ Template.orderLists.events({
 			});
 			Router.go('orderLists', { _id: currentOrderListId });
 		}
+	},
+
+	'click .edit-order': function(e) {
+		e.preventDefault();
+
+		var orderId = $(e.target).attr('data-id');
+		Session.set('selectedOrderId', orderId);
+		$('#edit-order-modal').modal('show');
+	}
+});
+
+Template.editOrderForm.helpers({
+	order: function() {
+		var orderId = Session.get('selectedOrderId');
+		return Orders.findOne({ _id: orderId });
+	},
+
+	menu: function() {
+		var orderId = Session.get('selectedOrderId');
+		var order = Orders.findOne({ _id: orderId });
+		return Menus.findOne({ _id: order.menuId });
+	}
+});
+
+Template.editOrderForm.events({
+	'click #edit-order-confirm-button': function(e) {
+		e.preventDefault();
+
+		var orderProperties = {
+			quantity: parseInt($('#edit_order_quantity').val())
+		}
+
+		var orderId = $('#edit_order_id').val();
+
+		if (orderProperties.quantity <= 0) {
+			var error = new Meteor.Error(422, 'Please specify a valid quantity');
+			Errors.throw(error.reason);
+		} else {
+			Orders.update({ _id: orderId }, { $set: orderProperties }, function(error) {
+				if (error) {
+					Errors.throw(error.reason);
+				} else {
+					Router.go('orderLists', { _id: currentOrderListId });
+				}
+			});
+		}
+
+		$('#edit-order-modal').modal('hide');
 	}
 });
